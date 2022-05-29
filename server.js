202875ff4696612ea2a4960e2142a66c12c75ebc
rimonspi12 <rimonmpi@gmail.com>
2022-05-29 18:29:52 +0600

    remove some spech

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bitd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
 // main functionally start here
 
 async function run() {
